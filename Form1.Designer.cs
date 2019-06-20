0d10e25dbd91912802800f2ea8fbb2b269acfc68
overwatch <overwatch11m99@gmail.com>
2019-06-21 02:12:43 +0430

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace howto_trominoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnTile = new System.Windows.Forms.Button();
+            this.picBoard = new System.Windows.Forms.PictureBox();
+            this.time = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(36, 14);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(47, 20);
+            this.txtN.TabIndex = 1;
+            this.txtN.Text = "3";
+            this.txtN.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnTile
+            // 
+            this.btnTile.Location = new System.Drawing.Point(104, 12);
+            this.btnTile.Name = "btnTile";
+            this.btnTile.Size = new System.Drawing.Size(75, 23);
+            this.btnTile.TabIndex = 2;
+            this.btnTile.Text = "Tile";
+            this.btnTile.UseVisualStyleBackColor = true;
+            this.btnTile.Click += new System.EventHandler(this.btnTile_Click);
+            // 
+            // picBoard
+            // 
+            this.picBoard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBoard.BackColor = System.Drawing.Color.White;
+            this.picBoard.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBoard.Location = new System.Drawing.Point(12, 41);
+            this.picBoard.Name = "picBoard";
+            this.picBoard.Size = new System.Drawing.Size(923, 444);
+            this.picBoard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picBoard.TabIndex = 3;
+            this.picBoard.TabStop = false;
+            this.picBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.picBoard_Paint);
+            this.picBoard.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picBoard_MouseClick);
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Location = new System.Drawing.Point(215, 17);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(0, 13);
+            this.time.TabIndex = 4;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(272, 14);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 5;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(434, 17);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(480, 17);
+            this.hScrollBar1.TabIndex = 6;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnTile;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(947, 497);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.picBoard);
+            this.Controls.Add(this.btnTile);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "howto_trominoes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Form1_Scroll);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button btnTile;
+        private System.Windows.Forms.PictureBox picBoard;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace howto_trominoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static bool @null = true;
+        private static float clickedx, clickedy;
+        private enum Quadrants { NW, NE, SE, SW };
+
+        private int SquaresPerSide = 0;
+        private float SquareWidth, BoardWidth, Xmin, Ymin;
+
+        // The location of the missing square.
+        private int MissingX, MissingY;
+
+        private List<PointF[]> Chairs;
+
+        // Make and solve a new board.
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            MakeBoard();
+        }
+        private void btnTile_Click(object sender, EventArgs e)
+        {
+            var date1 = DateTime.Now;
+            MakeBoard();
+            var date2 = DateTime.Now;
+            time.Text = (date2 - date1).Milliseconds.ToString();
+            //makeimage();
+        }
+        private  void MakeBoard()
+        {
+            int n = int.Parse(txtN.Text);
+            SquaresPerSide = (int)Math.Pow(2, n);
+
+            // Set board parameters.
+            //SquareWidth = (picBoard.ClientSize.Width - 10f) / SquaresPerSide;
+            //float hgt = (picBoard.ClientSize.Height - 10f) / SquaresPerSide;
+            //if (SquareWidth > hgt) SquareWidth = hgt;
+            SquareWidth = 5;
+            BoardWidth = SquareWidth * SquaresPerSide;
+            picBoard.Width = picBoard.Height = Convert.ToInt32(BoardWidth + 10f);
+            Xmin = (picBoard.ClientSize.Width - BoardWidth) / 2;
+            Ymin = (picBoard.ClientSize.Height - BoardWidth) / 2;
+
+            // Pick a random empty square.
+            if (@null)
+            {
+                Random rand = new Random();
+                MissingX = rand.Next(SquaresPerSide);
+                MissingY = rand.Next(SquaresPerSide);
+            }
+            else
+            {
+                float s = 0;
+                clickedx -= Xmin;
+                for (int i = 0;; i++)
+                {
+                    s += SquareWidth;
+                    if (s > clickedx)
+                    {
+                        MissingX = i;
+                        break;
+                    }
+                }
+
+                s = 0;
+                clickedy -= Ymin;
+                for (int j = 0;; j++)
+                {
+                    s += SquareWidth;
+                    if (s > clickedy)
+                    {
+                        MissingY = j;
+                        break;
+                    }
+                }
+            }
+
+            // Solve the board.
+            Chairs = new List<PointF[]>();
+
+            SolveBoard(
+                0, SquaresPerSide - 1,
+                0, SquaresPerSide - 1,
+                MissingX, MissingY);
+
+            //makeimage();
+
+            picBoard.Refresh();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            picBoard.Image.Save(@"D:\terme4\tarahi\howto_trominoes\aaa.jpeg", ImageFormat.Jpeg);
+        }
+
+        public Task makeimage()
+        {
+            // Redraw.
+            Bitmap bt = new Bitmap(picBoard.Width, picBoard.Width);
+            using (Graphics g = Graphics.FromImage(bt))
+            {
+                //g.Clear(bt.BackColor);
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                // Draw the chairs.
+                foreach (PointF[] points in Chairs)
+                {
+                    //Thread.Sleep(1000);
+                    g.DrawPolygon(Pens.Red, points);
+
+                }
+
+                // Draw the missing square.
+                float xx = Xmin + MissingX * SquareWidth;
+                float yy = Ymin + MissingY * SquareWidth;
+                g.FillRectangle(Brushes.Black,
+                    xx, yy, SquareWidth, SquareWidth);
+            }
+            picBoard.Image = bt;
+            bt.Save(@"D:\terme4\tarahi\howto_trominoes\aaa.jpeg", ImageFormat.Jpeg);
+            return Task.CompletedTask;
+
+
+        }
+
+        private void Form1_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+        }
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+        }
+
+        private void picBoard_MouseClick(object sender, MouseEventArgs e)
+        {
+            @null = false;
+            clickedx = e.X;
+            clickedy = e.Y;
+            MakeBoard();
+        }
+
+        // Solve the board.
+        private void SolveBoard(int imin, int imax, int jmin, int jmax,
+            int imissing, int jmissing)
+        {
+            // See if this is a 2x2 square.
+            if (imax - imin == 1)
+            {
+                // It is a 2x2 square. Make its chair.
+                Chairs.Add(MakeChair(imin, imax, jmin, jmax,
+                    imissing, jmissing));
+                return;
+            }
+
+            // Not a 2x2 square. Divide into 4 pieces and recurse.
+            int imid = (imin + imax) / 2;
+            int jmid = (jmin + jmax) / 2;
+            switch (QuadrantToIgnore(imin, imax, jmin, jmax,
+                imissing, jmissing))
+            {
+                case Quadrants.NW:
+                    // Make the chair in the middle.
+                    Chairs.Add(MakeChair(imid, imid + 1, jmid, jmid + 1, imid, jmid));
+
+                    // Recurse.
+                    SolveBoard(imin, imid, jmin, jmid, imissing, jmissing);         // NW
+                    SolveBoard(imid + 1, imax, jmin, jmid, imid + 1, jmid);         // NE
+                    SolveBoard(imid + 1, imax, jmid + 1, jmax, imid + 1, jmid + 1); // SE
+                    SolveBoard(imin, imid, jmid + 1, jmax, imid, jmid + 1);         // SW
+                    break;
+                case Quadrants.NE:
+                    // Make the chair in the middle.
+                    Chairs.Add(MakeChair(imid, imid + 1, jmid, jmid + 1, imid + 1, jmid));
+
+                    // Recurse.
+                    SolveBoard(imin, imid, jmin, jmid, imid, jmid);                 // NW
+                    SolveBoard(imid + 1, imax, jmin, jmid, imissing, jmissing);     // NE
+                    SolveBoard(imid + 1, imax, jmid + 1, jmax, imid + 1, jmid + 1); // SE
+                    SolveBoard(imin, imid, jmid + 1, jmax, imid, jmid + 1);         // SW
+                    break;
+                case Quadrants.SE:
+                    // Make the chair in the middle.
+                    Chairs.Add(MakeChair(imid, imid + 1, jmid, jmid + 1, imid + 1, jmid + 1));
+
+                    // Recurse.
+                    SolveBoard(imin, imid, jmin, jmid, imid, jmid);                 // NW
+                    SolveBoard(imid + 1, imax, jmin, jmid, imid + 1, jmid);         // NE
+                    SolveBoard(imid + 1, imax, jmid + 1, jmax, imissing, jmissing); // SE
+                    SolveBoard(imin, imid, jmid + 1, jmax, imid, jmid + 1);         // SW
+                    break;
+                case Quadrants.SW:
+                    // Make the chair in the middle.
+                    Chairs.Add(MakeChair(imid, imid + 1, jmid, jmid + 1, imid, jmid + 1));
+
+                    // Recurse.
+                    SolveBoard(imin, imid, jmin, jmid, imid, jmid);                 // NW
+                    SolveBoard(imid + 1, imax, jmin, jmid, imid + 1, jmid);         // NE
+                    SolveBoard(imid + 1, imax, jmid + 1, jmax, imid + 1, jmid + 1); // SE
+                    SolveBoard(imin, imid, jmid + 1, jmax, imissing, jmissing);     // SW
+                    break;
+            }
+        }
+
+        // Make a chair polygon.
+        private PointF[] MakeChair(int imin, int imax,
+            int jmin, int jmax, int imissing, int jmissing)
+        {
+            // Make the initial points.
+            float xmin = Xmin + imin * SquareWidth;
+            float ymin = Ymin + jmin * SquareWidth;
+            PointF[] points =
+            {
+                new PointF(xmin, ymin),
+                new PointF(xmin + SquareWidth, ymin),
+                new PointF(xmin + SquareWidth * 2, ymin),
+                new PointF(xmin + SquareWidth * 2, ymin + SquareWidth),
+                new PointF(xmin + SquareWidth * 2, ymin + SquareWidth * 2),
+                new PointF(xmin + SquareWidth, ymin + SquareWidth * 2),
+                new PointF(xmin, ymin + SquareWidth * 2),
+                new PointF(xmin, ymin + SquareWidth),
+            };
+
+            // Push in the appropriate corner.
+            PointF middle = new PointF(
+                xmin + SquareWidth,
+                ymin + SquareWidth);
+            switch (QuadrantToIgnore(imin, imax, jmin, jmax,
+                imissing, jmissing))
+            {
+                case Quadrants.NW:
+                    points[0] = middle;
+                    break;
+                case Quadrants.SW:
+                    points[6] = middle;
+                    break;
+                case Quadrants.NE:
+                    points[2] = middle;
+                    break;
+                case Quadrants.SE:
+                    points[4] = middle;
+                    break;
+            }
+
+            return points;
+        }
+
+        // Draw the board.
+        private  void picBoard_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(picBoard.BackColor);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Draw the chairs.
+            foreach (PointF[] points in Chairs)
+            {
+                e.Graphics.DrawPolygon(Pens.Red, points);
+            }
+
+            // Draw the missing square.
+            float x = Xmin + MissingX * SquareWidth;
+            float y = Ymin + MissingY * SquareWidth;
+            e.Graphics.FillRectangle(Brushes.Black,
+                x, y, SquareWidth, SquareWidth);
+        }
+
+        // Return the quadrant holding the square to be ignored for this square.
+        private Quadrants QuadrantToIgnore(int imin, int imax, int jmin, int jmax,
+            int imissing, int jmissing)
+        {
+            int imid = (imin + imax) / 2;
+            int jmid = (jmin + jmax) / 2;
+            if (imissing <= imid)      // West.
+            {
+                if (jmissing <= jmid) return Quadrants.NW;
+                return Quadrants.SW;
+            }
+            else                        // East.
+            {
+                if (jmissing <= jmid) return Quadrants.NE;
+                return Quadrants.SE;
+            }
+        }
+    }
+}
